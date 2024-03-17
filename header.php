0f1a35b343f5438398d110ee8391140f96ebab80
spark <98943801+Sonar135@users.noreply.github.com>
2024-03-17 14:15:40 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,114 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="https://kit.fontawesome.com/b782cf5553.js" crossorigin="anonymous"></script>  
+    <link rel="icon" type="image/x-icon" href="images\kisspng-babcock-university-university-of-ibadan-academic-d-5b1c90eb26da71.7889147215285987631592-removebg-preview.png">
+    <link rel="stylesheet" href="css/header.css?v=<?php echo time();?>">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Irental</title>
+</head>
+<body>
+
+<div class="loader">
+  <img src="images\8-removebg-preview.png" alt="">
+</div>
+    <div class="nav">
+      <div class="upper_nav">
+        <div class="upper_cent">
+          <div class="contact">
+            <div class="contact_card " id="address">
+              <div class="ico">
+              <i class="fa-solid fa-location-dot"></i>
+              </div>
+
+             Babcock University
+            </div>
+
+            <div class="contact_card phone" >
+              <div class="ico">
+              <i class="fa-solid fa-phone"></i>
+              </div>
+
+            08109495127
+            </div>
+
+            <div class="contact_card email" >
+              <div class="ico">
+              <i class="fa-regular fa-envelope"></i>
+              </div>
+
+          vefidi135@gmail.com
+            </div>
+
+          
+          </div>
+
+          <div class="social">
+          <i class="fa-brands fa-facebook-f"></i>
+          <i class="fa-brands fa-twitter"></i>
+          <i class="fa-brands fa-google-plus-g"></i>
+          <i class="fa-brands fa-pinterest"></i>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="lower_nav">
+        <div class="lower_cent">
+          <div class="logo">
+
+          </div>
+
+          <div class="menu">
+            <ul>
+              <li>
+              <a href=""> Home</a> 
+              </li>
+
+              <li>
+               <a href="">About Us</a> 
+              </li>
+
+              <li>
+               <a href=""> Vehicles</a>
+              </li>
+
+              <li>
+                <a href="">     Blog</a>
+              </li>
+
+              <li>
+             <a href="">  Contact</a>
+              </li>
+
+              <li>
+            <a href="">   Register/Login</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+</body>
+<script src="js/min.js"></script>
+<script src="js/dom.js"></script>
+
+<script>
+  $(window).on("load", ()=>{
+    $(".loader").fadeOut("slow")
+    $("body").css("overflow-y", "scroll");
+  });
+</script>
+</html>
+
+
+
+
+
+
---
+++ main.php
@@ -0,0 +1,105 @@
+<?php
+
+include "header.php";
+
+?>
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/main.css?v=<?php echo time();?>">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="hero">
+        <div class="container sec1">
+            <div class="left_btn">
+            <i class="fa-solid fa-chevron-left"></i>
+            </div>
+
+            <div class="right_btn">
+            <i class="fa-solid fa-angle-right"></i>
+            </div>
+            <div class="cent">
+                <div class="carousel">
+                    <h1>THE AMAZING RIDE</h1>
+
+                  <div class="display">
+                    <div class="text">
+                        <div class="text_box">
+                            <h1>HYUNDAI</h1>
+                            <h1>VELOSTER</h1>
+                            <h1 class="space">₦2000/hr</h1>
+                            <button>Book Now</button>
+                        </div>
+                    </div>
+
+                    <div class="img">
+                        <div class="car_img">
+                            <img src="images\download.png" alt="">
+                        </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="container sec2">
+            <h1>OUR SERVICES</h1>
+
+            <h4>We are offering a variety of cars, services, and partnerships to meet all your travel needs.</h4>
+            <div class="cent">
+                <div class="text_container">
+                    <div class="service_card">
+                        <div class="card_ico"><i class="fa-solid fa-key"></i></div>
+
+                        <div class="service">Pickup & Delivery</div>
+
+                        <div class="ser_desc">Mus accumsan venenatis hac curabitur per quis parturient vel ut a sit a socii.</div>
+                    </div>
+
+                    <div class="service_card">
+                             <div class="card_ico"><i class="fa-solid fa-key"></i></div>
+
+                        <div class="service">Pickup & Delivery</div>
+
+                        <div class="ser_desc">Mus accumsan venenatis hac curabitur per quis parturient vel ut a sit a socii.</div>
+                        </div>
+                </div>
+
+                <div class="pic_cont">
+                    <img src="images\car.png" alt="">
+                </div>
+
+                <div class="text_container right_side">
+                <div class="service_card">
+                        <div class="card_ico"><i class="fa-solid fa-key"></i></div>
+
+                        <div class="service">Pickup & Delivery</div>
+
+                        <div class="ser_desc">Mus accumsan venenatis hac curabitur per quis parturient vel ut a sit a socii.</div>
+                    </div>
+
+                    <div class="service_card">
+                             <div class="card_ico"><i class="fa-solid fa-key"></i></div>
+
+                        <div class="service">Pickup & Delivery</div>
+
+                        <div class="ser_desc">Mus accumsan venenatis hac curabitur per quis parturient vel ut a sit a socii.</div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
